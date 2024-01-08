0dc7e45819ce11ae62b066a87d6c22fa6759d43c
tatuya06 <155950768+tatuya06@users.noreply.github.com>
2024-01-08 17:13:16 +0900

    Create 1-6.py

---
+++ 第一章/2回目/1-6.py
@@ -0,0 +1,44 @@
+import random
+
+hands = ['グー','チョキ','パー']
+results ={'win':'勝ち','lose':'負け','draw':'あいこ'}
+
+#じゃんけんを開始するメッセージを表示する関数
+def start_message():
+    print('それでは、じゃんけんを開始します')
+
+#自身の手を決める関数
+def myhand():
+    print('あなたの手を入力してください')
+    my_hand = int(input('0:'+ hands[0]+'、1:'+ hands[1] +'、2:'+hands[2]))
+    return my_hand
+
+#相手の手を決める関数
+def youhand():
+    you_hand = random.randint(0,2)
+    return you_hand
+
+#お互いの手を表示する関数
+def view_hand(mh,yh):
+    print('あなたの手は' + hands[mh] + 'です')
+    print('相手の手は' + hands[yh] + 'です')
+
+#勝敗を決める関数
+def result(mh,yh):
+    hand_result = mh - yh
+    if hand_result == 0:
+        return 'draw'
+    elif hand_result == 2 or hand_result == -1:
+        return 'win'
+    elif hand_result == 1 or hand_result == -2:
+        return 'lose'
+
+def view_result(rs):
+    print(results[rs]+ 'です')
+    
+start_message()
+mh = myhand()
+yh = youhand()
+view_hand(mh,yh)
+rs = result(mh,yh)
+view_result(rs)
